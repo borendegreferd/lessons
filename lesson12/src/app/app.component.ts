c8c38e2ee553e031fec3845e43f56c33b587bece
trem2ry <borendegreferd@gmail.com>
2019-10-08 22:07:57 +0300

    без рутинга, немного по-другому создаю новый кеомпонент, если критично - переделаю к следующему. также доделаю рутинг

--- lesson12/src/app/app.component.ts
+++ lesson12/src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {Component} from '@angular/core';
-
-export interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-  date?: any;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -13,18 +6,5 @@ export interface Todo {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  appTitle = 'TodoList';
-
-  public todos: Todo[] = [
-    {id: 1, title: ' task', completed: false, date: new Date()},
-    {id: 2, title: ' shopping', completed: true, date: new Date()},
-    {id: 3, title: ' meeting', completed: false, date: new Date()},
-  ];
-
-  onToggle(id: number) {
-    console.log(id); // Продемонстрировать возможность подписки на изменения компонентов выводом соответствующего текста в консоль.
-    const idx = this.todos.findIndex(t => t.id === id);
-    this.todos[idx].completed = !this.todos[idx].completed
-  }
+  appTitle = 'Angular todo application'
 }
